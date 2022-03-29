ac53699a08ac5e0ac0f27dec029eabc7591777ae
junyoung <jy940608@naver.com>
2022-03-30 01:52:19 +0900

    ADD: admin restaurant

--- table_bookings/web/admin.py
+++ table_bookings/web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Restaurant
 
-# Register your models here.
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'address')
+    fieldsets = [
+        ('주요정보', {'fields': ['name', 'category', 'address', 'phone', 'visible']}),
+        ('상세정보', {'fields': ['menu_info', 'description']})
+    ]
